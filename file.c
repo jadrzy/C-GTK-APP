98ce972a9af226bdd6396ea8054478b2c487e265
Jan <jdrzyzga10@gmail.com>
2023-06-07 20:23:58 +0200

    l

--- file.c
+++ file.c
@@ -1,25 +1,15 @@
+#include <stdio.h>
 #include <gtk/gtk.h>
-
-static void activate (GtkApplication* app, gpointer user_data)
-{
-  GtkWidget *window;
-
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 1000, 800);
-  gtk_widget_show_all (window);
+static void activate (GtkApplication *app, gpointer user_data){
+// ADD PROPERTIES
 }
 
-int
-main (int argc, char **argv)
-{
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
-
-  return status;
+int main (int argc, char **argv) {
+GtkApplication *app;
+int ret;
+app = gtk_application_new ("App", G_APPLICATION_FLAGS_NONE);
+g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
+ret = g_application_run (G_APPLICATION(app), argc, argv);
+g_object_unref (app);
+return ret;
 }
